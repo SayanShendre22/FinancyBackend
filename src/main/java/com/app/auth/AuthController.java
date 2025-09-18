84b9e2d928a6e033074c60c8d062a25d7c0eb648
User <sayanshendreedu@gmail.com>
2025-09-19 01:02:11 +0530

    first commit

--- src/main/java/com/app/auth/AuthController.java
+++ src/main/java/com/app/auth/AuthController.java
@@ -39,7 +39,7 @@ public class AuthController {
 
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello this is an test API";
+		return "Hello this is an test API  ";
 	}
 
 	@RequestMapping("/register")
