0e0ebbba1cf6e28545924fe3cba2938d9d3f3da1
User <sayanshendreedu@gmail.com>
2025-09-22 16:46:16 +0530

    checking CICD pipeline

--- src/main/java/com/app/auth/AuthController.java
+++ src/main/java/com/app/auth/AuthController.java
@@ -41,6 +41,11 @@ public class AuthController {
 	public String hello() {
 		return "Hello this is an test API  ";
 	}
+	
+	@RequestMapping("/demo")
+	public String hello1() {
+		return "Just testing our API and CICD pipeline flow.";
+	}
 
 	@RequestMapping("/register")
 	public ResponseEntity<TokenModel> register(@RequestBody SignUpUser user) {
